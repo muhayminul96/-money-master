f3243d2038cb381ab2ebd77018e1a4bf3f39e5af
muhayminul <muhayminul96@gmail.com>
2022-02-17 20:42:09 +0600

    input value prosseor added with validation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,41 @@
 
 
+// input velue prossseing
+
+function inputProsseing(id){
+    inputText = document.getElementById(id).value;
+        if(isNaN(inputText)){
+            alert('please check ' + id + ' field , it need a number' )
+        }
+
+        else{
+            inputValue = parseFloat(inputText)
+
+            if(inputValue >= 0){
+                return inputValue;
+                
+            }
+            else{
+                alert('please check ' + id + ' field , it need a positive number' + inputValue)
+            }
+
+        }
+    
+}
+
+
+
 // using calculate exience and income
 
 document.getElementById('calculator').addEventListener('click',function(){
-    document.getElementById('balance').innerText = 3000
-    incomeI
+    incomeValue = inputProsseing('income');
+    foodValue = inputProsseing('food');
+    rentValue = inputProsseing('rent');
+    clothValue   = inputProsseing('cloth');
+
+    
+
+
+    console.log(incomeValue)
 })
 
